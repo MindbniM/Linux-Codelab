68b4b42ce3ade98b77f9c9c61c02fb3466951434
MindbniM <1771392560@qq.com>
2024-03-15 21:50:05 +0800

    myshell1.5 可以实现cd

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -4,25 +4,33 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
-void command()
-{
-    const char* s=getenv("USER");
-    const char* h=getenv("HOSTNAME");
-    const char* bin=getenv("PWD");
-    printf("[%s@%s %s]$ ",s,h,bin);
-}
 #define SIZE 1024
 #define MAX_COM 64
-void strto(char* argv[])
+char pwd[SIZE];
+const char* ENVname(const char* name)
+{
+    const char* s=getenv(name);
+    if(s==NULL) return "None";
+    return s;
+}
+void command()
+{
+    printf("[%s@%s %s]$ ",ENVname("USER"),ENVname("HOSTNAME"),ENVname("PWD"));
+}
+int strto(char* argv[],char* buff)
 {
     
-    char buff[SIZE];
     fgets(buff,SIZE,stdin);
     buff[strlen(buff)-1]=0;
     int i=0;
     argv[i++]=strtok(buff," ");
     while(argv[i++]=strtok(NULL," "));
-
+    if(strcmp(argv[0], "ls") ==0)
+    {
+        argv[i-1] = (char*)"--color";
+        argv[i] = NULL;
+    }
+    return strlen(buff);
 
 }
 void exe(char* argv[])
@@ -35,15 +43,38 @@ void exe(char* argv[])
     }
     waitpid(id,NULL,0);
 }
+int built(char* argv[])
+{
+    int ret=1;
+    if(strcmp(argv[0],"cd")==0)
+    {
+        ret=0;
+        const char* tar=argv[1];
+        if(tar==NULL)
+        {
+            tar=ENVname("HOME");
+        }
+        chdir(tar);
+        char temp[SIZE];
+        getcwd(temp,SIZE);
+        snprintf(pwd,SIZE,"PWD=%s",temp);
+        putenv(pwd);
+    }
+    return ret;
+}
 int main()
 {
     while(1)
     {
         char* argv[MAX_COM];
         
+        char buff[SIZE];
         command();
-        strto(argv);
+        int n= strto(argv,buff);
+        if(n==0) continue;
+        n=built(argv);
+        if(n==0) continue;
         exe(argv); 
-        return 0;
     }
+        return 0;
 }
